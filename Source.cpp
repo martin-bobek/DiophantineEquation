86b3d472a7c1c583001d1b304c1e811223bd0bed
martin-bobek <martinbobek8@gmail.com>
2019-01-05 12:20:18 -0700

    Added function to compute a minimal solution in X given a D to the Diophantine equation.

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,24 @@
+#include <array>
+#include <iostream>
 
+size_t minX(size_t D);
 
 int main() {
+	std::array<size_t, 5> Ds = { 2, 3, 5, 6, 7 };
 
+	for (auto D : Ds)
+		std::cout << "(D = " << D << ") -> (X = " << minX(D) << ')' << std::endl;
+}
+
+size_t minX(size_t D) {
+	for (size_t x = 2;; x++) {
+		for (size_t y = 1;; y++) {
+			size_t lhs = x * x;
+			size_t rhs = D * y * y + 1;
+			if (lhs == rhs)
+				return x;
+			if (lhs < rhs)
+				break;
+		}
+	}
 }
